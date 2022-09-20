ec9441286e0b80d0da55f03b2de422f603668d06
Veronica <veronicagoranova@gmail.com>
2022-09-20 23:51:29 +0300

    Initial create.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero?: Hero;
+  heroes = HEROES;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+}
